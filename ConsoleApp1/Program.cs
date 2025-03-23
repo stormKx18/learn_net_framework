9d03853bb39dacbd07883705c37daf56d50e8e70
stormKx18 <vicjaime922@gmail.com>
2025-03-23 08:10:05 -0600

    New console app with nuget package

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+using Pastel;
+
+Console.WriteLine("Hello, World!".Pastel(Color.Yellow));
--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b03f6bf3392b096bd072cd265d91d467af771e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6543add9be67d11b678731f0e7766ad2523754b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
